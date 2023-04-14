bb5872eb6663da9431cb4fd953cbe102239d0e19
AndresCarnicero <acarni01@ucm.es>
2023-04-14 19:39:59 +0200

    XMLSerializer pero no esta integrado

--- Neon_Rider/Assets/Scripts/SistemaTelemetria/XMLPrueba.cs
+++ Neon_Rider/Assets/Scripts/SistemaTelemetria/XMLPrueba.cs
@@ -4,62 +4,63 @@ using UnityEngine;
 using System.Xml;
 using System.Xml.Serialization;
 using System.IO;
+using System.Threading.Tasks;
 
-//Clase auxiliar para serializar Vector2
-public class xmlVector2
-{
-    string x;
-    string y;
+////Clase auxiliar para serializar Vector2
+//public class xmlVector2
+//{
+//    string x;
+//    string y;
 
-    public bool IsEmpty()
-    {
-        return string.IsNullOrEmpty(x) && string.IsNullOrEmpty(y);
-    }
+//    public bool IsEmpty()
+//    {
+//        return string.IsNullOrEmpty(x) && string.IsNullOrEmpty(y);
+//    }
     
-    public xmlVector2(float x_, float y_)
-    {
-        x = x_.ToString();
-        y = y_.ToString();
-    }
-    public xmlVector2() { }
-}
+//    public xmlVector2(float x_, float y_)
+//    {
+//        x = x_.ToString();
+//        y = y_.ToString();
+//    }
+//    public xmlVector2() { }
+//}
 
-// Objeto preset para serializar xml
-// Tiene todos los parámetros de todos los eventos
-public class DataXmlObject
-{
-    // Variables comunes a todos los eventos
-    public string x_type;
-    public string x_time;
+//// Objeto preset para serializar xml
+//// Tiene todos los parámetros de todos los eventos
+//public class DataXmlObject
+//{
+//    // Variables comunes a todos los eventos
+//    public string x_type;
+//    public string x_time;
 
-    // Variables diferentes de cada evento
-    public string? x_enemyId { get; set; }
-    public string? x_roomId { get; set; }
-    public xmlVector2? x_pos { get; set; }
+//    // Variables diferentes de cada evento
+//    public string? x_enemyId { get; set; }
+//    public string? x_roomId { get; set; }
+//    public xmlVector2? x_pos { get; set; }
 
-    // Por cada variable de evento hay que espeficiar si se debe omitir cuando es null
-    [XmlIgnore]
-    public bool ShouldSerializeEnemyId
-    {
-        get { return !string.IsNullOrEmpty(x_enemyId); }
+//    // Por cada variable de evento hay que espeficiar si se debe omitir cuando es null
+//    [XmlIgnore]
+//    public bool ShouldSerializeEnemyId
+//    {
+//        get { return !string.IsNullOrEmpty(x_enemyId); }
 
-    }
+//    }
 
-    [XmlIgnore]
-    public bool ShouldSerializeRoomId
-    {
-        get { return !string.IsNullOrEmpty(x_roomId); }
+//    [XmlIgnore]
+//    public bool ShouldSerializeRoomId
+//    {
+//        get { return !string.IsNullOrEmpty(x_roomId); }
 
-    }
+//    }
 
-    [XmlIgnore]
-    public bool ShouldSerializePos
-    {
-        get { return x_pos.IsEmpty(); }
+//    [XmlIgnore]
+//    public bool ShouldSerializePos
+//    {
+//        get { return x_pos.IsEmpty(); }
 
-    }
+//    }
 
-}
+//}
 
 public class XMLPrueba : MonoBehaviour
 {
@@ -87,7 +88,6 @@ public class XMLPrueba : MonoBehaviour
         xmlStream = new StreamWriter("Prueba.xml");
         serializer.Serialize(xmlStream, dataXml);
 
-
     }
 
     // Update is called once per frame
@@ -95,4 +95,5 @@ public class XMLPrueba : MonoBehaviour
     {
         
     }
+
 }
---
+++ Neon_Rider/Assets/Scripts/SistemaTelemetria/XMLSerializer.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+//Clase auxiliar para serializar Vector2
+public class xmlVector2
+{
+    string x;
+    string y;
+
+    public bool IsEmpty()
+    {
+        return string.IsNullOrEmpty(x) && string.IsNullOrEmpty(y);
+    }
+
+    public xmlVector2(float x_, float y_)
+    {
+        x = x_.ToString();
+        y = y_.ToString();
+    }
+    public xmlVector2() { }
+}
+
+// Objeto preset para serializar xml
+// Tiene todos los parámetros de todos los eventos
+public class DataXmlObject
+{
+    // Variables comunes a todos los eventos
+    public string x_type;
+    public string x_time;
+
+    // Variables diferentes de cada evento
+    public string? x_enemyId { get; set; }
+    public string? x_roomId { get; set; }
+    public xmlVector2? x_pos { get; set; }
+
+    // Por cada variable de evento hay que espeficiar si se debe omitir en la serializacion cuando es null
+    [XmlIgnore]
+    public bool ShouldSerializeEnemyId { get { return !string.IsNullOrEmpty(x_enemyId); } }
+
+    [XmlIgnore]
+    public bool ShouldSerializeRoomId { get { return !string.IsNullOrEmpty(x_roomId); } }
+
+    [XmlIgnore]
+    public bool ShouldSerializePos { get { return x_pos.IsEmpty(); } }
+
+}
+
+public class XMLSerializer : ISerializer
+{
+    public override async Task<string> Serialize(TrackerEvent e)
+    {
+        string t = e.GetEventType();
+
+        // Creacion del objeto DataXml
+        DataXmlObject dataXml = new DataXmlObject();
+        StringWriter stringWriter = new StringWriter();
+        XmlSerializer serializer = new XmlSerializer(typeof(DataXmlObject));
+
+        // Atributos comunes a todos los eventos
+        dataXml.x_type = t;
+        dataXml.x_time = e.getTimeStamp().ToString();
+
+        // Atributos específicos a cada evento
+        switch (t)
+        {
+            case "Muerte Enemigo":
+                dataXml.x_pos = new xmlVector2(e.getVar().pos.Value.x, e.getVar().pos.Value.y);
+                dataXml.x_enemyId = e.getVar().enemyId.ToString();
+                break;
+            case "Muerte Jugador":
+            case "Bloqueo":
+            case "Posicion Jugador":
+            case "Posicion NPC":
+                dataXml.x_pos = new xmlVector2(e.getVar().pos.Value.x, e.getVar().pos.Value.y);
+                break;
+            case "Inicio Sala":
+            case "Fin Sala":
+                dataXml.x_roomId = e.getVar().enemyId.ToString();
+                break;
+        }
+
+        // Lo serializamos con el formato en una variable
+        serializer.Serialize(stringWriter, dataXml);
+
+        // La devolvemos como string
+        return await Task.FromResult(stringWriter.ToString());
+    }
+}
